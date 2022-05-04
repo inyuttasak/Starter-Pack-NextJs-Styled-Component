8206c0a964bd616b6f466bab1bae295501028a86
Inyuttasak <in117dk54@gmail.com>
2022-05-04 08:40:03 +0700

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Nav,NavContainer} from '../styles/styled-components/NavbarStyle'
+
+const Navbar = () => {
+  return (
+   <Nav>
+       <NavContainer>
+           
+       </NavContainer>
+   </Nav>
+  )
+}
+
+export default Navbar
